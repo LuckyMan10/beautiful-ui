81504f44295ef0126e04e0c9bb6cb952407036ca
LuckyMan10 <danil.shemyakin.01@list.ru>
2022-01-21 20:53:07 +0500

    added toggle-buttons

--- src/main.js
+++ src/main.js
@@ -4,5 +4,6 @@ import "./components/date-dropdown/index";
 import "./components/subscription-field/index";
 import "./components/checkbox-buttons/index";
 import "./components/radio-buttons/index";
+import "./components/toggle-buttons/index";
 
 import "./app.scss";
